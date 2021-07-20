9b28e052230606c79e3424edf13527257af02cb3
Diana Pennycook <dkpennycook@gmail.com>
2021-07-19 23:47:03 -0500

    end of hw19 - hover states
    added hover states using jquery to top nav and button on homepage

---
+++ js/index.js
@@ -0,0 +1,131 @@
+console.log('index.js file is loaded');
+
+
+/*hover text on top nav*/
+$(".headerText1").mouseover(function(){
+    $(".headerText1").css("color", "#ffe2a9");
+});
+$(".headerText1").mouseout(function(){
+    $(".headerText1").css("color", "white");
+});
+
+$(".headerText2").mouseover(function(){
+    $(".headerText2").css("color", "#ffe2a9");
+});
+$(".headerText2").mouseout(function(){
+    $(".headerText2").css("color", "white");
+});
+
+$(".headerText3").mouseover(function(){
+    $(".headerText3").css("color", "#ffe2a9");
+});
+$(".headerText3").mouseout(function(){
+    $(".headerText3").css("color", "white");
+});
+
+$(".headerText4").mouseover(function(){
+    $(".headerText4").css("color", "#ffe2a9");
+});
+$(".headerText4").mouseout(function(){
+    $(".headerText4").css("color", "white");
+});
+
+/*hover text on footer nav*/
+
+$(".footerText1").mouseover(function(){
+    $(".footerText1").css("color", "#ffe2a9");
+});
+$(".footerText1").mouseout(function(){
+    $(".footerText1").css("color", "white");
+});
+
+$(".footerText2").mouseover(function(){
+    $(".footerText2").css("color", "#ffe2a9");
+});
+$(".footerText2").mouseout(function(){
+    $(".footerText2").css("color", "white");
+});
+
+$(".footerText3").mouseover(function(){
+    $(".footerText3").css("color", "#ffe2a9");
+});
+$(".footerText3").mouseout(function(){
+    $(".footerText3").css("color", "white");
+});
+
+$(".footerText4").mouseover(function(){
+    $(".footerText4").css("color", "#ffe2a9");
+});
+$(".footerText4").mouseout(function(){
+    $(".footerText4").css("color", "white");
+});
+
+$(".footerText5").mouseover(function(){
+    $(".footerText5").css("color", "#ffe2a9");
+});
+$(".footerText5").mouseout(function(){
+    $(".footerText5").css("color", "white");
+});
+
+$(".footerText6").mouseover(function(){
+    $(".footerText6").css("color", "#ffe2a9");
+});
+$(".footerText6").mouseout(function(){
+    $(".footerText6").css("color", "white");
+});
+
+$(".footerText7").mouseover(function(){
+    $(".footerText7").css("color", "#ffe2a9");
+});
+$(".footerText7").mouseout(function(){
+    $(".footerText7").css("color", "white");
+});
+
+/*hover on HERO buttons*/
+$(".heroButton").mouseover(function(){
+    $(".heroButton").css("background-color", "#2c8dc4");
+    $(".heroButton").css("color", "white");
+});
+$(".heroButton").mouseout(function(){
+    $(".heroButton").css("background-color", "white");
+    $(".heroButton").css("color", "#2c8dc4");
+});
+
+/*hover on CASE STUDY buttons*/
+
+$(".EPAbutton").mouseover(function(){
+    $(".EPAbutton").css("background-color", "#2c8dc4");
+    $(".EPAbutton").css("color", "white");
+});
+$(".EPAbutton").mouseout(function(){
+    $(".EPAbutton").css("background-color", "white");
+    $(".EPAbutton").css("color", "#2c8dc4");
+});
+
+$(".LGbutton").mouseover(function(){
+    $(".LGbutton").css("background-color", "#2c8dc4");
+    $(".LGbutton").css("color", "white");
+});
+$(".LGbutton").mouseout(function(){
+    $(".LGbutton").css("background-color", "white");
+    $(".LGbutton").css("color", "#2c8dc4");
+});
+
+$(".HangryButton").mouseover(function(){
+    $(".HangryButton").css("background-color", "#2c8dc4");
+    $(".HangryButton").css("color", "white");
+});
+$(".HangryButton").mouseout(function(){
+    $(".HangryButton").css("background-color", "white");
+    $(".HangryButton").css("color", "#2c8dc4");
+});
+
+/*smooth scroll*/
+/****unable to figure this one out at this time */
+/*$(document).ready(function() {
+    $("fas fa-chevron-circle-down fa-5x").on('click', function(event) {
+        if(this.#)
+    })
+}*/
+
+
